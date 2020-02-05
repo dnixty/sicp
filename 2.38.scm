1f1eb6b259aa238f99f392a467d7bfffb7820c39
Dominik Stodolny <dominik@stodolny.org>
2020-02-05 21:26:25 +0000

    solve 2.33-2.41

---
+++ 2.38.scm
@@ -0,0 +1,28 @@
+(define (fold-right op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(fold-right / 1 (list 1 2 3))
+;; 3/2
+
+(fold-left / 1 (list 1 2 3))
+;; 1/6
+
+(fold-right list '() (list 1 2 3))
+;; (1 (2 (3 ())))
+
+(fold-left list '() (list 1 2 3))
+;; (((() 1) 2) 3)
+
+;; To guarantee that `fold-right' and `fold-left' will produce the
+;; same values, `op' has to be commutative and associative.
